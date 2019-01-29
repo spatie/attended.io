7e128dfa6acb5324173c0729135223bc02a2ecab
freek <freek@spatie.be>
2019-01-29 21:58:21 +0100

    install telescope

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
