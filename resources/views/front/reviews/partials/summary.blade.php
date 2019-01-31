b2f6d88838052e9d7de5056c82d4652180a5c0ba
freek <freek@spatie.be>
2019-01-31 08:40:33 +0100

    wip

--- resources/views/front/reviews/partials/summary.blade.php
+++ resources/views/front/reviews/partials/summary.blade.php
@@ -1,2 +1,2 @@
-{{ $reviewable->number_of_reviews }} {{ str_plural($reviewable->number_of_reviews, 'review') }}<br />
+{{ $reviewable->number_of_reviews }} {{ str_plural('review', $reviewable->number_of_reviews) }}<br />
 Average rating: {{ $reviewable->average_review_rating }}
