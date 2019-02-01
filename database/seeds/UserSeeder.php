472e5789ca5723e71bac1d52ffde5a30d60124e6
freek <freek@spatie.be>
2019-02-02 00:33:00 +0100

    wip

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -7,7 +8,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         foreach (range(1, 100) as $i) {
-            factory(\App\Models\User::class)->create([
+            factory(User::class)->create([
                'email' => "user{$i}@attended.io",
             ]);
         }
