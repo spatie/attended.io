18ca11a19d0d771ae658f1a9402ca044f1b2694c
freek <freek@spatie.be>
2019-02-05 00:13:19 +0100

    preload reviews

--- app/Http/Front/Controllers/Slots/ShowSlotController.php
+++ app/Http/Front/Controllers/Slots/ShowSlotController.php
@@ -8,6 +8,11 @@ class ShowSlotController
 {
     public function __invoke(Slot $slot)
     {
+        $slot->load([
+            'reviews',
+            'reviews.user',
+        ]);
+
         return view('front.slots.show', compact('slot'));
     }
 }
