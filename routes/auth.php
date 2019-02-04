81d20f76188eec0bd083b846b0d9dc5fb95ee025
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-02-04 22:17:31 +0100

    Add register routes

--- routes/auth.php
+++ routes/auth.php
@@ -2,12 +2,16 @@
 
 use App\Http\Front\Controllers\Auth\ForgotPasswordController;
 use App\Http\Front\Controllers\Auth\LoginController;
+use App\Http\Front\Controllers\Auth\RegisterController;
 use App\Http\Front\Controllers\Auth\ResetPasswordController;
 use App\Http\Front\Controllers\Auth\VerificationController;
 
 Route::middleware('guest')->group(function () {
     Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('login', [LoginController::class, 'login']);
+
+    Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('register', [RegisterController::class, 'register']);
 
     Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
     Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
