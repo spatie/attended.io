4827e101cf07c4c1b8c899470c0ff1fe622d74e7
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-02-04 23:11:29 +0100

    Wip creating events

--- resources/views/front/event-admin/events/create.blade.php
+++ resources/views/front/event-admin/events/create.blade.php
@@ -5,12 +5,14 @@
 
     <form action="{{ route('event-admin.events.store') }}" method="POST">
         @csrf
+
         <input type="text" name="name">
         <textarea name="description"></textarea>
         <input type="text" name="location">
         <input type="text" name="website">
         <input type="date" name="starts_at">
         <input type="date" name="ends_at">
+
         <button>Create</button>
     </form>
 @endsection
