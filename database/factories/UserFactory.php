1b5c1fa9ab4fdcd3755ff23fd75d34c1cee073c3
freek <freek@spatie.be>
2019-02-12 09:52:58 +0100

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,6 +11,11 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
         'admin' => $faker->boolean(10),
+        'email_verified_at' => $faker->dateTime(),
         'can_create_events_immediately' => $faker->boolean(20),
     ];
 });
+
+$factory->state(Event::class, 'unverified-email', [
+    'email_verified_at' => null,
+]);
--- tests/Feature/Actions/UpdateUserActionTest.php
+++ tests/Feature/Actions/UpdateUserActionTest.php
@@ -4,10 +4,19 @@ namespace Tests\Feature\Actions;
 
 use App\Actions\UpdateUserAction;
 use App\Models\User;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Facades\Notification;
 use Tests\TestCase;
 
 class UpdateUserActionTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        Notification::fake();
+    }
+
     /** @test */
     public function it_can_update_a_user()
     {
@@ -20,5 +29,39 @@ class UpdateUserActionTest extends TestCase
 
         $this->assertEquals('updated-name', $user->name);
         $this->assertEquals('new-user@example.com', $user->email);
+    }
+
+    /** @test */
+    public function the_email_address_will_stay_verified_if_it_did_not_change()
+    {
+        $user = factory(User::class)->create();
+
+        $this->assertTrue($user->hasVerifiedEmail());
+
+        (new UpdateUserAction())->execute($user, [
+            'name' => 'updated-name',
+            'email' => $user->email,
+        ]);
+
+        $this->assertTrue($user->hasVerifiedEmail());
+
+        Notification::assertNothingSent();
+    }
+
+    /** @test */
+    public function the_email_address_will_need_to_verified_again_if_it_did_change()
+    {
+        $user = factory(User::class)->create();
+
+        $this->assertTrue($user->hasVerifiedEmail());
+
+        (new UpdateUserAction())->execute($user, [
+            'name' => 'updated-name',
+            'email' => 'updated-' . $user->email,
+        ]);
+
+        $this->assertFalse($user->hasVerifiedEmail());
+
+        Notification::assertSentTo($user, VerifyEmail::class);
     }
 }
