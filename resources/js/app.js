d941539e4b1179e3897ad9e23807e994201f1239
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-02-21 09:29:46 +0100

    Reset scripts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-import Turbolinks from 'turbolinks';
-import { Application } from 'stimulus';
-import { definitionsFromContext } from 'stimulus/webpack-helpers';
+// import Turbolinks from 'turbolinks';
 
-Turbolinks.start();
-
-const application = Application.start();
-const context = require.context('./controllers', true, /\.js$/);
-application.load(definitionsFromContext(context));
+// Turbolinks.start();
--- resources/views/front/events/partials/filter-bar.blade.php
+++ resources/views/front/events/partials/filter-bar.blade.php
@@ -7,25 +7,4 @@
             <a href="?past=1">Past</a>
         </li>
     </ul>
-    <form
-        id="search-form"
-        data-controller="search"
-        data-turbolinks-permanent
-    >
-        <input
-            type="text"
-            name="query"
-            placeholder="Search..."
-            value="{{ request()->query('query') }}"
-            data-target="search.input"
-            data-action="keyup->search#submit"
-        >
-        <button
-            data-target="search.clear"
-            data-action="search#reset"
-            class="{{ request()->query('query') ? null : 'hidden' }}"
-        >
-            Clear
-        </button>
-    </form>
 </nav>
