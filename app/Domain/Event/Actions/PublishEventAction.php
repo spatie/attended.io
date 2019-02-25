10a56bbeeea2324bf72a4b2f46dd6cace010fe38
freek <freek@spatie.be>
2019-02-25 08:23:43 +0100

    wip

--- app/Domain/Event/Actions/PublishEventAction.php
+++ app/Domain/Event/Actions/PublishEventAction.php
@@ -3,11 +3,16 @@
 namespace App\Domain\Event\Actions;
 
 use App\Domain\Event\Models\Event;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class PublishEventAction
 {
+    use AuthorizesRequests;
+
     public function execute(Event $event): Event
     {
+        $this->authorize('publish', $event);
+
         $event->markAsPublished();
 
         return $event;
