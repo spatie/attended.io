bbce74716a7fc2e1e31608aa9ad0b9cb5d6356dd
freek <freek@spatie.be>
2019-03-01 21:59:26 +0100

    wip

--- resources/views/front/event-admin/slots/index.blade.php
+++ resources/views/front/event-admin/slots/index.blade.php
@@ -5,7 +5,6 @@
 
     <a href="{{ route('event-admin.slots.create', $event) }}">Create new slot</a>
 
-
     <h1>Slots for {{ $event->name }}</h1>
 
     <a href="{{ route('event-admin.slots.create', $event->id) }}" class="underline">Add new slot</a>
