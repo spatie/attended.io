690135f848beff29444a429b540c0bedda37d5ef
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-03-14 11:11:30 +0100

    Add permalinks to reviews

--- app/Domain/Review/Models/Review.php
+++ app/Domain/Review/Models/Review.php
@@ -2,8 +2,12 @@
 
 namespace App\Domain\Review\Models;
 
+use App\Domain\Event\Models\Event;
 use App\Domain\Shared\Models\BaseModel;
+use App\Domain\Slot\Models\Slot;
 use App\Domain\User\Models\User;
+use App\Http\Front\Controllers\Events\ShowEventFeedbackController;
+use App\Http\Front\Controllers\Slots\ShowSlotController;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
@@ -22,5 +26,18 @@ class Review extends BaseModel
     public function reviewableType(): string
     {
         return lcfirst(class_basename($this->reviewable_type));
+    }
+
+    public function getPermalink(): string
+    {
+        if ($this->reviewable_type === Slot::class) {
+            $url = action(ShowSlotController::class, $this->reviewable_id);
+        }
+
+        if ($this->reviewable_type === Event::class) {
+            $url = action(ShowEventFeedbackController::class, $this->reviewable_id);
+        }
+
+        return "$url#review{$this->id}";
     }
 }
--- resources/views/front/reviews/partials/item.blade.php
+++ resources/views/front/reviews/partials/item.blade.php
@@ -1,14 +1,16 @@
-<div>
-{{ $review->user->name }} {{ $review->updated_at->diffForHumans() }} <br/>
+<div id="review{{ $review->id }}">
+    {{ $review->user->name }}
+    <a href="{{ $review->getPermalink() }}">{{ $review->updated_at->diffForHumans() }}</a>
+    <br/>
 
-Rating {{ $review->rating }}<br/>
+    Rating {{ $review->rating }}<br/>
 
-{{ $review->remarks }}<br/>
+    {{ $review->remarks }}<br/>
 
-@can('delete', $review)
+    @can('delete', $review)
         <action-button :action="route('reviews.delete', $review->id)">
             @method('DELETE')
             <button type="submit">Delete review</button>
         </action-button>
-@endcan
+    @endcan
 </div>
