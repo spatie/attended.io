5dea8e817f10db2aa68c744467117b98a5915f12
Luddinus <luddinus@gmail.com>
2019-03-23 14:57:00 +0100

    Use Relation MorphMap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use App\Domain\Event\Models\Event;
+use App\Domain\Slot\Models\Slot;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        Relation::morphMap([
+            Event::class => 'Event',
+            Slot::class => 'Slot'
+        ]);
     }
 }
