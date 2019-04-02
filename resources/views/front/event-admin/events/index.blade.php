18afb2c2f0e328a1b469741d83aad4485af18b18
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-04-02 23:05:50 +0200

    Fix event admin index

--- resources/views/front/event-admin/events/index.blade.php
+++ resources/views/front/event-admin/events/index.blade.php
@@ -5,17 +5,20 @@
 
     <a href="{{ route('event-admin.events.create') }}">Host new event</a>
 
-    {{ $events->links() }}
-    <div class="flex -m-4">
-        @foreach($events as $event)
-            <a href="{{ route('event-admin.events.edit', $event->idSlug()) }}" class="block bg-blue-lightest m-4">
-                <div class="p-3">
-                    <h2 class="font-bold">{{ $event->name }}</h2>
-                    {{ $event->timespan() }}<br>
-                    {{ $event->location }}
-                </div>
-            </>
-        @endforeach
-    </div>
-    {{ $events->links() }}
+    @if($events->isNotEmpty())
+        {{ $events->links() }}
+
+        <div class="flex -m-4">
+            @foreach($events as $event)
+                <a href="{{ route('event-admin.events.edit', $event->idSlug()) }}" class="block bg-blue-lightest m-4">
+                    <div class="p-3">
+                        <h2 class="font-bold">{{ $event->name }}</h2>
+                        {{ $event->timespan() }}<br>
+                        {{ $event->location }}
+                    </div>
+                </>
+            @endforeach
+        </div>
+        {{ $events->links() }}
+    @endif
 @endsection
